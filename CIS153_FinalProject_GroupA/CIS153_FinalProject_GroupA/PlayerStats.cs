a098d4387fbfba69b614ac15b8f0d5a8cf4af825
bongardam <74200328+bongardam@users.noreply.github.com>
2021-04-12 19:21:24 -0400

    Class and File Update - AB
    Created stats class and text files for individual stats.

---
+++ CIS153_FinalProject_GroupA/CIS153_FinalProject_GroupA/PlayerStats.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS153_FinalProject_GroupA
+{
+    public class PlayerStats
+    {
+        int playerOneMoves;
+        int playerOneGames;
+        int playerOneOutcome;
+        int playerTwoMoves;
+        int playerTwoGames;
+        int playerTwoOutcome;
+        int compMoves;
+        int compGames;
+        int compOutcome;
+
+        public PlayerStats(int pm, int pg, int po, int tm, int tg, int to, int cm, int cg, int co)
+        {
+            playerOneMoves = pm;
+            playerOneGames = pg;
+            playerOneOutcome = po;
+            playerTwoMoves = tm;
+            playerTwoGames = tg;
+            playerTwoOutcome = to;
+            compMoves = cm;
+            compGames = cg;
+            compOutcome = co;
+        }
+
+        public int getPlayerOneMoves()
+        {
+            return playerOneMoves;
+        }
+
+        public int getPlayerOneGames()
+        {
+            return playerOneGames;
+        }
+
+        public int getPlayerOneOutcome()
+        {
+            return playerOneOutcome;
+        }
+
+        public int getPlayertwoMoves()
+        {
+            return playerTwoMoves;
+        }
+
+        public int getPlayerTwoGames()
+        {
+            return playerTwoGames;
+        }
+
+        public int getPlayerTwoOutcome()
+        {
+            return playerTwoOutcome;
+        }
+
+        public int getCompMoves()
+        {
+            return compMoves;
+        }
+
+        public int getCompGames()
+        {
+            return compGames;
+        }
+
+        public int getCompOutcome()
+        {
+            return compOutcome;
+        }
+
+
+
+
+    }
+}
--- CIS153_FinalProject_GroupA/CIS153_FinalProject_GroupA/Properties/Resources.Designer.cs
+++ CIS153_FinalProject_GroupA/CIS153_FinalProject_GroupA/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CIS153_FinalProject_GroupA.Properties
-{
-
-
+namespace CIS153_FinalProject_GroupA.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,71 @@ namespace CIS153_FinalProject_GroupA.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CIS153_FinalProject_GroupA.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 0,0,0.
+        /// </summary>
+        internal static string CompStats {
+            get {
+                return ResourceManager.GetString("CompStats", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 0,0,0.
+        /// </summary>
+        internal static string PlayerOneStats {
+            get {
+                return ResourceManager.GetString("PlayerOneStats", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 0,0,0.
+        /// </summary>
+        internal static string PlayerTwoStats {
+            get {
+                return ResourceManager.GetString("PlayerTwoStats", resourceCulture);
             }
         }
     }
